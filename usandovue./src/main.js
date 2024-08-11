9f4561d92180f078432218655b4b41c180fd6a9a
Galdinowski <arthurlazarogaldino@gmail.com>
2024-08-11 19:56:39 -0300

    Definindo o Pinia

--- usandovue./src/main.js
+++ usandovue./src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
-import store from './store'
+import { createPinia } from 'pinia'
+const app = createApp(App);
+const pinia = createPinia(); 
+app.use(pinia);
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
